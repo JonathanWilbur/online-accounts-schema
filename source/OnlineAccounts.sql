40b3610955f6e71c1aaf055eb16afe67357fd919
Jonathan M. Wilbur <jonathan@wilbur.space>
2019-03-02 16:04:34 -0500

    Added 'serviceDescription' field

--- source/OnlineAccounts.sql
+++ source/OnlineAccounts.sql
@@ -39,6 +39,7 @@ CREATE TABLE IF NOT EXISTS OnlineAccounts
     `governmentRelated`                 BOOLEAN COMMENT 'Whether the account is for a service that the government operates, such as the Department of Motor Vehicles (DMV).',
     `employerRelated`                   BOOLEAN COMMENT 'Whether the account was set up or is managed by the owner''s employer.',
     `hasMoney`                          BOOLEAN COMMENT 'Whether the account holds the owner''s money.',
+    `serviceDescription`                TEXT COMMENT 'A description of the service or the account held with the service.',
     `notes`                             TEXT
 );
 
